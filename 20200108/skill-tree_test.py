168d1e63c6f565baeb10ca97c87435d4d87c0295
KwakCena <rhkrgudwh@gmail.com>
2020-01-08 18:20:43 +0900

    skill tree 다시 풀어보기 1
    어제 코딩도장에서 풀어본 skill tree 문제를 다시 풀어봣다.
    
    1. 먼저 skill_tree하나에 skill 이외의 문자들은 제거한 뒤 ''.join(list)로 문자열을 만들었다.
    skill_tree 안에 skill의 단어가 하나도 없으면 빈 문자열을 만든다. 이를 수행하는 메서드는
    del_other_skill()로 명한다.
    
    2. skill 문자열이 새롭게 바뀐 skill_tree 문자열로 시작하는지 판단한다. 이는 startswith() 메서드로 동작한다.
    새롭게 바뀐 skill_tree는 skill의 시작과 같지 않으면 무조건 배울 수 없는 skill이다.
    
    3. True/False를 list에 담아 sum하면 True의 합을 반환한다.

--- 20200108/skill-tree_test.py
+++ 20200108/skill-tree_test.py
@@ -1,6 +1,40 @@
 def solution(skill, skill_trees):
-    return 2
+    return sum(
+        [is_valid_skill_tree(skill, del_other_skill(skill, skill_tree))
+         for skill_tree in skill_trees]
+    )
+
+
+def del_other_skill(skill, skill_tree):
+    set_skill = set(skill)
+    return ''.join([ch for ch in skill_tree if ch in set_skill])
+
+
+def is_valid_skill_tree(skill, changed_skill_tree):
+    return skill.startswith(changed_skill_tree)
 
 
 def test_solution():
     assert solution("CBD", ["BACDE", "CBADF", "AECB", "BDA"]) == 2
+    assert solution("CBD", ["CABDE", "CBADF", "AECB", "BDA"]) == 3
+
+
+def test_del_other_skill():
+    assert del_other_skill("CBD", "BACDE") == "BCD"
+    assert del_other_skill("CBD", "CBADF") == "CBD"
+    assert del_other_skill("CBD", "AECB") == "CB"
+    assert del_other_skill("CBD", "BDA") == "BD"
+    assert del_other_skill("CBD", "AF") == ""
+
+
+def test_is_valid_skill_tree():
+    assert is_valid_skill_tree("CBD", "BCD") is False
+    assert is_valid_skill_tree("CBD", "CB") is True
+    assert is_valid_skill_tree("CBD", "B") is False
+    assert is_valid_skill_tree("CBD", "CBD") is True
+    assert is_valid_skill_tree("CBD", "BD") is False
+    assert is_valid_skill_tree("CBD", "") is True
+    assert is_valid_skill_tree("ABC", "A") is True
+    assert is_valid_skill_tree("ABC", "AB") is True
+    assert is_valid_skill_tree("ABC", "ABC") is True
+    assert is_valid_skill_tree("ABC", "B") is False
