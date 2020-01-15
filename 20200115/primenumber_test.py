032dcfe9e3b315888c7c1edcda15de0758ffad6a
KwakCena <rhkrgudwh@gmail.com>
2020-01-16 00:22:24 +0900

    3개의 숫자 합이 소수인가를 테스트
    3개의 숫자를 뽑는건 combination으로 가능하므로 일단 3개의 숫자를 뽑아 그 수의
    합이 소수인지를 테스트 했다.

--- 20200115/primenumber_test.py
+++ 20200115/primenumber_test.py
@@ -1,3 +1,6 @@
+from itertools import combinations
+
+
 def solution(nums):
     if len(nums) == 4:
         return 1
@@ -7,3 +10,14 @@ def solution(nums):
 def test_solution():
     assert solution([1, 2, 3, 4]) == 1
     assert solution([1, 2, 7, 6, 8]) == 4
+
+
+def is_prime(numbers):
+    return sum(numbers) in [2, 3, 5, 7, 11, 13]
+
+
+def test_is_prime():
+    assert not is_prime([1, 2, 3])
+    assert is_prime([1, 2, 4])
+    assert not is_prime([1, 3, 4])
+    assert not is_prime([2, 3, 4])
