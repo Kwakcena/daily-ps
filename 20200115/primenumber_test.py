98e4c1ed2af02313c2cbc05a36300f33388cd6f3
KwakCena <rhkrgudwh@gmail.com>
2020-01-16 01:37:01 +0900

    Create prime number list
    에라토스테네스의 체를 이용하여 소수 목록을 구하는 primes 메서드 작성

--- 20200115/primenumber_test.py
+++ 20200115/primenumber_test.py
@@ -6,13 +6,28 @@ def solution(nums):
 
 
 def is_prime(numbers):
-    return sum(numbers) in [2, 3, 5, 7, 11, 13, 17]
+    return sum(numbers) in primes(sorted(numbers))
 
 
 def prime_case(numbers):
     sorted_numbers = sorted(numbers)
     combinations_list = combinations(sorted_numbers, 3)
     return [list(xs) for xs in combinations_list if is_prime(xs)]
+
+
+def primes(sorted_numbers):
+    max_number = sum(sorted_numbers[-3:])
+    prime_numbers = set(range(2, max_number + 1))
+    for i in range(2, max_number + 1):
+        if i not in prime_numbers:
+            continue
+        prime_numbers -= set(range(i * 2, max_number + 1, i))
+    return list(prime_numbers)
+
+
+def test_primes():
+    assert primes([1, 2, 3, 4]) == [2, 3, 5, 7]
+    assert primes([1, 2, 4, 6, 7]) == [2, 3, 5, 7, 11, 13, 17]
 
 
 def test_solution():
