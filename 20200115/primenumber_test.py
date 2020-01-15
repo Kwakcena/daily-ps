599a8078ef93f9644812740d524d0263d4b80515
KwakCena <rhkrgudwh@gmail.com>
2020-01-16 01:10:36 +0900

    prime_case 메서드 테스트
    뽑은 3개의 숫자가 합이 소수인지 판단 후 소수이면 그 3개의 숫자 리스트를 원소로
    하는 리스트를 반환하는 메서드를 테스트 함.
    
    combinations_list에 numbers에서 3개씩 뽑은 목록들이 저장되어 있음.

--- 20200115/primenumber_test.py
+++ 20200115/primenumber_test.py
@@ -7,13 +7,13 @@ def solution(nums):
     return 4
 
 
+def is_prime(numbers):
+    return sum(numbers) in [2, 3, 5, 7, 11, 13, 17]
+
+
 def test_solution():
     assert solution([1, 2, 3, 4]) == 1
-    assert solution([1, 2, 7, 6, 8]) == 4
-
-
-def is_prime(numbers):
-    return sum(numbers) in [2, 3, 5, 7, 11, 13]
+    assert solution([1, 2, 7, 6, 4]) == 4
 
 
 def test_is_prime():
@@ -21,3 +21,16 @@ def test_is_prime():
     assert is_prime([1, 2, 4])
     assert not is_prime([1, 3, 4])
     assert not is_prime([2, 3, 4])
+
+
+def prime_case(numbers):
+    sorted_numbers = sorted(numbers)
+    combinations_list = combinations(sorted_numbers, 3)
+    return [list(xs) for xs in combinations_list if is_prime(xs)]
+
+
+def test_prime_case():
+    assert prime_case([1, 2, 3, 4]) == [[1, 2, 4]]
+    assert prime_case([1, 2, 7, 6, 4]) == [
+        [1, 2, 4], [1, 4, 6], [2, 4, 7], [4, 6, 7]
+    ]
