90839c98ff7a01ce7fd25728f7f0a62dde34a24a
KwakCena <rhkrgudwh@gmail.com>
2020-01-16 11:48:45 +0900

    Solve prime numbers
    소수 만들기 문제를 다시 풀었다.
    
    아샬님과 했을 땐 prime_case에서 is_prime함수를 조건 검사할때마다 불렀다.
    근데 그럴 필요 없이 is_prime 메서드를 지우고 리스트 컴프레션 안에다가
    is_prime을 합쳐놓으면 매번 소수의 리스트를 만들어놓을 필요가 없을 거라
    생각되어 바꿔봤다.

--- 20200116/primenumber_test.py
+++ 20200116/primenumber_test.py
@@ -1,9 +1,49 @@
+from itertools import combinations
+
+
 def solution(numbers):
-    if len(numbers) == 4:
-        return 1
-    return 4
+    return len(prime_case(numbers))
+
+
+def primes(sorted_numbers):
+    max_number = sum(sorted_numbers[-3:])
+    prime_numbers = set(range(2, max_number + 1))
+    for i in range(2, max_number + 1):
+        if i not in prime_numbers:
+            continue
+        prime_numbers -= set(range(i * 2, max_number + 1, i))
+    return prime_numbers
+
+
+def prime_case(numbers):
+    sorted_numbers = sorted(numbers)
+    prime_numbers = primes(sorted_numbers)
+    return [
+        xs
+        for xs in combinations(sorted_numbers, 3)
+        if sum(xs) in prime_numbers
+    ]
+
+
+def test_primes():
+    assert primes([1, 2, 3, 4]) == {2, 3, 5, 7}
+    assert primes([1, 2, 4, 6, 7]) == {2, 3, 5, 7, 11, 13, 17}
+
+
+def test_prime_case():
+    assert prime_case([1, 2, 3, 4]) == [(1, 2, 4)]
 
 
 def test_solution():
     assert solution([1, 2, 3, 4]) == 1
     assert solution([1, 2, 7, 6, 4]) == 4
+
+
+def test_combinations():
+    assert list(combinations([1, 2, 3, 4], 3)) == [
+        (1, 2, 3), (1, 2, 4), (1, 3, 4), (2, 3, 4)
+    ]
+    assert list(combinations([1, 2, 4, 6, 7], 3)) == [
+        (1, 2, 4), (1, 2, 6), (1, 2, 7), (1, 4, 6), (1, 4, 7), (1, 6, 7),
+        (2, 4, 6), (2, 4, 7), (2, 6, 7), (4, 6, 7)
+    ]
