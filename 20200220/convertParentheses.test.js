53966d2ea204c4da2874f029232271e2c233d78e
Kwakcena <rhkrgudwh@gmail.com>
2020-02-21 13:50:57 +0900

    Update code with code review from 한윤석
    윤석님의 코드 리뷰에 따라 코드를 수정했다.
    
    - reverseBracket
    Side effect란 "컴퓨터 과학에서 함수가 결과값 이외에 다른 상태를 변경시킬
    때 부작용이 있다고 말한다" 를 뜻한다. 기존의 reverseBracket 함수는 map을
    통해서 외부의 변수인 reversed 배열의 요소를 수정했다. side effect 를
    고려하여 외부의 데이터를 수정하지 않도록 코드를 수정했다.
    
    - isUpright에서 some의 사용.
    some은 테스트가 하나라도 통과하면 true, 하나도 통과하지 않는다면 false를
    반환한다. 여기서 하나라도 통과하여 true를 반환하면 iteration을 바로
    종료하게 된다.
    
    예를 들어 다음과 같은 테스트 케이스가 있다고 하자. [')', '(']
    1. 처음으로 나온 괄호 ')'로 인해서 bracketCount 은 -1이다.
    2. true를 반환하면서 iteration을 바로 종료. some은 true를 반환
    3. !로 인해 isUpright 함수는 some이 반환한 결과의 반대인 false 반환.
    
    - convertParentheses 함수 for loop의 복잡함.
    for loop 안에서 괄호의 갯수가 맞는 순간에 upright, balanced라는 이름으로
    문제의 u, v를 받아냈다. 이 과정을 split라는 함수로 분리하면서, 좀 더
    깔끔하게 만들어주었다.
    
    여기서 split 함수는 배열의 메서드인 findIndex를 사용하는데, 이는 ture
    값을 반환하는 요소가 있을 경우 순차실행을 중지하고 해동 요소의 인덱스를
    반환한다. 이에 대해 split 함수는 분리된 문자열을 배열로 반환하여
    비구조화 할당 문법을 통해 convertParentheses 함수에서 받게 된다.
    
    또한 템플릿 문자열을 이용하면 더 편한것을 까먹고 안쓰고 있었다.

--- 20200220/convertParentheses.test.js
+++ 20200220/convertParentheses.test.js
@@ -1,66 +1,70 @@
 const solution = p => convertParentheses(p);
 
 function convertParentheses(brackets) {
-  if (isUpright(brackets)) return brackets;
-  if (!brackets) return "";
-
-  let bracketCount = { "(": 0, ")": 0 };
-  let [upright, balanced] = "";
-
-  for (let i = 0; i < brackets.length; i++) {
-    bracketCount[brackets[i]] += 1;
-    if (bracketCount["("] === bracketCount[")"]) {
-      upright = brackets.substr(0, i + 1);
-      balanced = brackets.substr(i + 1, brackets.lenght);
-      if (isUpright(upright)) {
-        return upright + convertParentheses(balanced);
-      }
-      return (
-        "(" +
-        convertParentheses(balanced) +
-        ")" +
-        reverseBracket(upright.substr(1, upright.length - 2))
-      );
-    }
+  if (isUpright(brackets)) {
+    return brackets;
   }
+
+  if (!brackets) {
+    return "";
+  }
+
+  const [upright, balanced] = split(brackets);
+
+  if (isUpright(upright)) {
+    return upright + convertParentheses(balanced);
+  }
+  return `
+    (
+    ${convertParentheses(balanced)}
+    )
+    ${reverseBracket(upright.slice(1, -1))}
+    `;
 }
 
-const isUpright = str => {
-  const strArr = str.split("");
-  let bracketCount = 0;
-  for (let i in strArr) {
-    if (strArr[i] === "(") {
-      bracketCount += 1;
-    } else if (strArr[i] === ")") {
-      bracketCount -= 1;
-    }
-    if (bracketCount < 0) return false;
-  }
-  return bracketCount === 0;
+const split = brackets => {
+  let count = 0;
+  const i = [...brackets].findIndex(it => {
+    count += it === "(" ? 1 : -1;
+    return count === 0;
+  });
+  return [brackets.slice(0, i + 1), brackets.slice(i + 1)];
 };
 
-const reverseBracket = str => {
-  const reversed = str.split("");
-  return reversed
-    .map((bracket, i) => (reversed[i] = bracket == ")" ? "(" : ")"))
-    .join("");
+test("split", () => {
+  expect(split("()(())")).toEqual(["()", "(())"]);
+});
+
+const isUpright = str => {
+  let bracketCount = 0;
+  return !str.split("").some(it => {
+    bracketCount += it === "(" ? 1 : -1;
+    if (bracketCount < 0) {
+      return true;
+    }
+  });
 };
+
+const reverseBracket = str =>
+  [...str].map(it => (it === ")" ? "(" : ")")).join("");
 
 test("reverseBracket", () => {
-  expect(reverseBracket("))))")).toBe("((((");
-  expect(reverseBracket("(())")).toBe("))((");
-  expect(reverseBracket("()()()")).toBe(")()()(");
-  expect(reverseBracket("(()))(((()()((()")).toBe("))((())))()()))(");
+  // expect(reverseBracket("))))")).toBe("((((");
+  // expect(reverseBracket("(())")).toBe("))((");
+  // expect(reverseBracket("()()()")).toBe(")()()(");
+  // expect(reverseBracket("(()))(((()()((()")).toBe("))((())))()()))(");
 });
 
 test("isUpright", () => {
-  expect(isUpright("()")).toBe(true);
-  expect(isUpright(")(")).toBe(false);
+  // expect(isUpright("()")).toBe(true);
+  // expect(isUpright("(((())))")).toBe(true);
+  expect(isUpright("(()))")).toBe(false);
+  // expect(isUpright(")(")).toBe(false);
 });
 
 test("convertParentheses", () => {
-  expect(convertParentheses(")(")).toBe("()");
-  expect(convertParentheses("")).toBe("");
-  expect(convertParentheses("(()())()")).toBe("(()())()");
-  expect(convertParentheses("()))((()")).toBe("()(())()");
+  // expect(convertParentheses(")(")).toBe("()");
+  // expect(convertParentheses("")).toBe("");
+  // expect(convertParentheses("(()())()")).toBe("(()())()");
+  // expect(convertParentheses("()))((()")).toBe("()(())()");
 });
