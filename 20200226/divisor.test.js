d85fce4b960f2fd6f3df24b735c46e5bfe3f5ec0
Kwakcena <rhkrgudwh@gmail.com>
2020-02-26 11:40:13 +0900

    Refactoring 나누어 떨어지는 숫자 배열
    filter라는 좋은 메서드를 두고 이상하게 풀었다.
    javascript 의 다양한 메서드 사용에 관해서 노션에 정리해놔야겠다.

--- 20200226/divisor.test.js
+++ 20200226/divisor.test.js
@@ -1,22 +1,10 @@
 const solution = (arr, divisor) => {
-  const answer = divisible(arr, divisor);
-  return answer.length ? answer : [-1];
+  const answer = arr.filter(element => element % divisor == 0);
+  return answer.length ? answer.sort((a, b) => a - b) : [-1];
 };
 
-const divisible = (arr, divisor) =>
-  arr
-    .reduce((answer, element) => {
-      element % divisor == 0 ? answer.push(element) : answer;
-      return answer;
-    }, [])
-    .sort((a, b) => a - b);
-
-test("divisible", () => {
-  expect(divisible([5, 9, 7, 10], 5)).toEqual([5, 10]);
-  expect(divisible([2, 36, 1, 3], 1)).toEqual([1, 2, 3, 36]);
-  expect(divisible([3, 2, 6], 10)).toEqual([]);
-});
-
 test("solution", () => {
+  expect(solution([5, 9, 7, 10], 5)).toEqual([5, 10]);
+  expect(solution([2, 36, 1, 3], 1)).toEqual([1, 2, 3, 36]);
   expect(solution([3, 2, 6], 10)).toEqual([-1]);
 });
