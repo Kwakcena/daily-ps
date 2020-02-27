8e90e7e7906fa8735c3a0892cdb5138e6adf68d3
Kwakcena <rhkrgudwh@gmail.com>
2020-02-27 14:48:31 +0900

    Solve 문자열 내 마음대로 정렬 problem using JS
    1. 이해
    - 문자열이 들어있는 배열의 문자열들을 정렬한다.
    - 주어진 인덱스 요소의 문자를 기준으로 정렬한다.
    2. 계획
    - sort 메서드를 사용한다. sort는 callback 함수를 받는다.
    - array.sort((a, b) => a - b) 시에 음의 값이면 a, 양의 값으면 b를
      반환한다.
    - 문자가 같을 경우에는 사전순으로 앞에 있는게 반환되어야 하므로 삼항
      연산자를 통해 -1 또는 1을 반환하도록 했다.
    3. 실행
    4. 반성
    - 다른 이의 풀이를 보니 localeCompare 라는 메서드가 있다. 사전순으로
      앞에오는지 뒤에오는지 비교해주는 함수라는데 사용해봐야겠다.

--- 20200227/strSortMyself.test.js
+++ 20200227/strSortMyself.test.js
@@ -1,7 +1,19 @@
-const solution = (strings, n) => {
-  return ["car", "bed", "sun"];
-};
+const solution = (strings, n) =>
+  strings.sort((r, l) =>
+    r[n] === l[n] ? (r < l ? -1 : 1) : r[n].charCodeAt(0) - l[n].charCodeAt(0)
+  );
 
 test("solution", () => {
   expect(solution(["sun", "bed", "car"], 1)).toEqual(["car", "bed", "sun"]);
+  expect(solution(["abce", "abcd", "cdx"], 2)).toEqual(["abcd", "abce", "cdx"]);
+  expect(solution(["aaaa", "aaba", "aaca"], 2)).toEqual([
+    "aaaa",
+    "aaba",
+    "aaca"
+  ]);
+  expect(solution(["aaaa", "aaba", "aaca"], 1)).toEqual([
+    "aaaa",
+    "aaba",
+    "aaca"
+  ]);
 });
