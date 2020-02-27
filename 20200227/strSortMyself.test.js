507e2046a76bfe2397fa5d30d9fc6ed7f20460ae
Kwakcena <rhkrgudwh@gmail.com>
2020-02-27 14:56:53 +0900

    Using Method 'localeCompare'
    두 개의 문자열을 사전순으로 비교.
    r.localeCompare(l) 에 대해서
    
    - r이 사전순으로 l보다 앞에 있으면 음의 값.
    - r이 사전순으로 l보다 뒤에 있으면 양의 값.
    - 같으면 0
    
    실행 결과는 localeCompare를 사용한 것이 더 느리다.

--- 20200227/strSortMyself.test.js
+++ 20200227/strSortMyself.test.js
@@ -3,6 +3,11 @@ const solution = (strings, n) =>
     r[n] === l[n] ? (r < l ? -1 : 1) : r[n].charCodeAt(0) - l[n].charCodeAt(0)
   );
 
+const solution2 = (strings, n) =>
+  strings.sort((r, l) =>
+    r[n] === l[n] ? r.localeCompare(l) : r[n].localeCompare(l[n])
+  );
+
 test("solution", () => {
   expect(solution(["sun", "bed", "car"], 1)).toEqual(["car", "bed", "sun"]);
   expect(solution(["abce", "abcd", "cdx"], 2)).toEqual(["abcd", "abce", "cdx"]);
@@ -12,6 +17,25 @@ test("solution", () => {
     "aaca"
   ]);
   expect(solution(["aaaa", "aaba", "aaca"], 1)).toEqual([
+    "aaaa",
+    "aaba",
+    "aaca"
+  ]);
+});
+
+test("solution2", () => {
+  expect(solution2(["sun", "bed", "car"], 1)).toEqual(["car", "bed", "sun"]);
+  expect(solution2(["abce", "abcd", "cdx"], 2)).toEqual([
+    "abcd",
+    "abce",
+    "cdx"
+  ]);
+  expect(solution2(["aaaa", "aaba", "aaca"], 2)).toEqual([
+    "aaaa",
+    "aaba",
+    "aaca"
+  ]);
+  expect(solution2(["aaaa", "aaba", "aaca"], 1)).toEqual([
     "aaaa",
     "aaba",
     "aaca"
