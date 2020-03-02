2becfbaef06f632807a815f29bd85b18d544da31
Kwakcena <rhkrgudwh@gmail.com>
2020-03-02 12:48:38 +0900

    Refactoring
    sort에 callback 넣고 정규식 쓰고 이럴 필요 없다.

--- 20200229/stringdescendingorder.test.js
+++ 20200229/stringdescendingorder.test.js
@@ -1,20 +1,9 @@
-const solution = s => `${extraction(s, /[a-z]/g)}${extraction(s, /[A-Z]/g)}`;
-
-const extraction = (str, pattern) => {
-  const extractionStr = str.match(pattern);
-  return extractionStr
-    ? extractionStr.sort((a, b) => (a > b ? -1 : 1)).join("")
-    : "";
-};
-
-test("extraction", () => {
-  expect(extraction("abcABC", /[A-Z]/g)).toBe("CBA");
-  expect(extraction("abcABC", /[a-z]/g)).toBe("cba");
-  expect(extraction("abcdez", /[a-z]/g)).toBe("zedcba");
-  expect(extraction("azbycx", /[a-z]/g)).toBe("zyxcba");
-  expect(extraction("a", /[a-z]/g)).toBe("a");
-  expect(extraction("a", /[A-Z]/g)).toBe("");
-});
+const solution = s =>
+  s
+    .split("")
+    .sort()
+    .reverse()
+    .join("");
 
 test("solution", () => {
   expect(solution("Zbcdefg")).toBe("gfedcbZ");
