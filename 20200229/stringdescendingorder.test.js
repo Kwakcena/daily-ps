3814c8e46759f7cfd6e6355dd2f19423d4f1e9b3
Kwakcena <rhkrgudwh@gmail.com>
2020-03-02 12:14:18 +0900

    정규식을 이용해 문자열 추출 후 합침.
    - 정규식을 이용해 소문자와 대문자만 따로 추출함.
    - match 메서드는 추출 한 문자를 담은 배열을 반환함.
    - 배열의 메서드인 reverse()를 통해 거꾸로 바꿈.
    
    * 여기서 착각이 있었다. reverse() 메서드가 알아서 아스키코드 숫자대로
      문자를 정렬 해 줄 것이라 생각했다.
    * reverse()는 담겨있는 배열의 원소를 순서상 거꾸로 바꿔줄 뿐이다.

--- 20200229/stringdescendingorder.test.js
+++ 20200229/stringdescendingorder.test.js
@@ -1,10 +1,19 @@
-const solution = s => {
-  return s
-    .split("")
+const solution = s => `${extraction(s, /[a-z]/g)}${extraction(s, /[A-Z]/g)}`;
+
+const extraction = (str, pattern) =>
+  str
+    .match(pattern)
     .reverse()
     .join("");
-};
+
+test("extraction", () => {
+  expect(extraction("abcABC", /[A-Z]/g)).toBe("CBA");
+  expect(extraction("abcABC", /[a-z]/g)).toBe("cba");
+  expect(extraction("abcdez", /[a-z]/g)).toBe("zedcba");
+  expect(extraction("azbycx", /[a-z]/g)).toBe("zyxcba");
+});
 
 test("solution", () => {
   expect(solution("Zbcdefg")).toBe("gfedcbZ");
+  expect(solution("abcdeABCDE")).toBe("edcbaEDCBA");
 });
