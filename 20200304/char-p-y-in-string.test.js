4095582e2e41fb5aafba1c2ea890d2053c389921
Kwakcena <rhkrgudwh@gmail.com>
2020-03-04 13:35:05 +0900

    Solve Using Regexp method 'match'
    정규표현식을 이용해 풀었다. 그 중 match을 이용해 y,Y의 개수와 p,P의
    개수를 샌 값을 배열에 담아 배열의 원소 개수가 같으면 true, 아니면 false
    를 반환한다.

--- 20200304/char-p-y-in-string.test.js
+++ 20200304/char-p-y-in-string.test.js
@@ -14,9 +14,19 @@ const solution1 = s => {
   return p_cnt === y_cnt;
 };
 
+const solution2 = s =>
+  (s.match(/p|P/g) || []).length === (s.match(/y|Y/g) || []).length;
+
 test("solution1", () => {
   expect(solution1("pPoooyY")).toBe(true);
   expect(solution1("Pyy")).toBe(false);
   expect(solution1("abc")).toBe(true);
   expect(solution1("a")).toBe(true);
+});
+
+test("solution2", () => {
+  expect(solution2("pPoooyY")).toBe(true);
+  expect(solution2("Pyy")).toBe(false);
+  expect(solution2("abc")).toBe(true);
+  expect(solution2("a")).toBe(true);
 });
