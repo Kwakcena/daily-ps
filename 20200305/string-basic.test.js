be7f613945aeee5bec221885a15db652de1b5b39
Kwakcena <rhkrgudwh@gmail.com>
2020-03-05 21:12:00 +0900

    Solve) 정규식 이용한 풀이
    ^ : 입력의 시작 부분에 대응되는 플래그.
    \d : 숫자 0~9
    {n} : 앞 표현식이 n번 나타나는지
    $ : 입력의 끝과 매칭

--- 20200305/string-basic.test.js
+++ 20200305/string-basic.test.js
@@ -2,6 +2,8 @@ const solution = s =>
   (s.length == 4 || s.length == 6) &&
   s.split("").every(v => (v >= "0") & (v <= "9"));
 
+const solution2 = s => /^\d{4}$|^\d{6}$/.test(s);
+
 test("solution", () => {
   expect(solution("a234")).toBe(false);
   expect(solution("1234")).toBe(true);
@@ -13,4 +15,12 @@ test("solution", () => {
   expect(solution("1e12")).toBe(false);
   expect(solution("abcd12")).toBe(false);
   expect(solution("0000")).toBe(true);
+});
+
+test("정규 표현식", () => {
+  expect(solution2("2345")).toBe(true);
+  expect(solution2("a223")).toBe(false);
+  expect(solution2("abcd")).toBe(false);
+  expect(solution2("123456")).toBe(true);
+  expect(solution2("12345678")).toBe(false);
 });
