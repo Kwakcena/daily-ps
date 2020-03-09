218f88acc216740f4b3d376a4150f6bd65bb7039
Kwakcena <rhkrgudwh@gmail.com>
2020-03-09 22:45:52 +0900

    Solve other solution
    자바스크립트에서 string에 사칙 연산을 적용하면 정수가 됨.

--- 20200309/str-to-int.test.js
+++ 20200309/str-to-int.test.js
@@ -2,8 +2,18 @@ const solution = s => {
   return parseInt(s);
 };
 
+const solution2 = s => {
+  return s / 1;
+};
+
 test("solution", () => {
   expect(solution("1234")).toBe(1234);
   expect(solution("-1234")).toBe(-1234);
   expect(solution("+1234")).toBe(+1234);
+});
+
+test("solution2", () => {
+  expect(solution2("1234")).toBe(1234);
+  expect(solution2("-1234")).toBe(-1234);
+  expect(solution2("+1234")).toBe(+1234);
 });
