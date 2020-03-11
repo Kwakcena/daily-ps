571253214247e6a0c6159898059dbfeee2266cc9
Kwakcena <rhkrgudwh@gmail.com>
2020-03-12 01:21:19 +0900

    Solve other way
    repeat와 slice를 사용함.

--- 20200311/subarksubark.test.js
+++ 20200311/subarksubark.test.js
@@ -6,7 +6,13 @@ const solution = n => {
   return str;
 };
 
+const solution2 = n => "수박".repeat(n).slice(0, n);
+
 test("solution", () => {
   expect(solution(3)).toBe("수박수");
   expect(solution(4)).toBe("수박수박");
+});
+
+test("solution2", () => {
+  expect(solution2(3)).toBe("수박수");
 });
