33afa9a80e24d8c7b3613744107c4728cf72b024
Kwakcena <rhkrgudwh@gmail.com>
2020-03-20 12:58:07 +0900

    Add solution2 (using splice, indexOf, ...arr)

--- 20200320/Remove-the-smallest-number/Remove-the-smallest-number.test.js
+++ 20200320/Remove-the-smallest-number/Remove-the-smallest-number.test.js
@@ -5,6 +5,11 @@ const solution = arr => {
 
 const findMinValue = arr => Math.min.apply(null, arr);
 
+const solution2 = arr => {
+  arr.splice(arr.indexOf(Math.min(...arr)), 1);
+  return arr.length < 1 ? [-1] : arr;
+};
+
 test("제일 작은 수 제거하기", () => {
   expect(solution([4, 3, 2, 1])).toEqual([4, 3, 2]);
   expect(solution([10])).toEqual([-1]);
@@ -13,4 +18,9 @@ test("제일 작은 수 제거하기", () => {
 test("최소값 찾기", () => {
   expect(findMinValue([4, 3, 2, 1])).toBe(1);
   expect(findMinValue([10])).toBe(10);
+});
+
+test("splice 사용 풀이", () => {
+  expect(solution2([4, 3, 2, 1])).toEqual([4, 3, 2]);
+  expect(solution2([10])).toEqual([-1]);
 });
