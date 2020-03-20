ca1da12fcc7f12ab5a71d6e9f43063d58f3eb9f6
Kwakcena <rhkrgudwh@gmail.com>
2020-03-20 11:57:26 +0900

    0은 match에서 빠져서 안들어가는 것으로 보임.
    match의 정규식 범위를 0-9로 바꿔야 한다.

---
+++ 20200320/Invert-numbers-into-an-array/Invert-numbers-into-an-array.test.js
@@ -0,0 +1,12 @@
+const solution = n => {
+  return String(n)
+    .match(/[1-9]/g)
+    .reverse()
+    .map(number => 0 | number);
+};
+
+test("자연수 뒤집어 배열로 만들기", () => {
+  expect(solution(12345)).toEqual([5, 4, 3, 2, 1]);
+  expect(solution(929384)).toEqual([4, 8, 3, 9, 2, 9]);
+  expect(solution(10000000000)).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
+});
