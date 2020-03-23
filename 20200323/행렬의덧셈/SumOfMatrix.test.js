5f8a2e9355c2485b62faf0a12782fefcb5933156
Kwakcena <rhkrgudwh@gmail.com>
2020-03-23 14:17:49 +0900

    성공) map을 이용한 1차원 배열 덧셈
    2차원 배열은 각 원소로 1차원 배열을 갖는다.
    서로 다른 2차원 배열의 원소들을 더해주는 sumMatrix 함수를 만들었다.
    
    map을 이용해서 1차원 배열의 원소를 순회하며 arr2의 index를 참조해 arr1의
    데이터와 더한 뒤 이를 배열로 반환해준다.

--- 20200323/행렬의덧셈/SumOfMatrix.test.js
+++ 20200323/행렬의덧셈/SumOfMatrix.test.js
@@ -1,12 +1,19 @@
 const solution = (arr1, arr2) => {
-  const answer = [[], []];
+  const answer = [];
   for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2[0].length; j++) {
-      answer[i][j] = arr1[i][j] + arr2[i][j];
-    }
+    answer.push(sumMatrix(arr1[i], arr2[i]));
   }
   return answer;
 };
+
+const sumMatrix = (arr1, arr2) => {
+  return [...arr1].map((val, idx) => val + arr2[idx]);
+};
+
+test("sum matrix", () => {
+  expect(sumMatrix([1, 2, 3], [4, 5, 6])).toEqual([5, 7, 9]);
+  expect(sumMatrix([3, 7, 4, 1], [9, 2, 3, 4])).toEqual([12, 9, 7, 5]);
+});
 
 test("solution", () => {
   expect(
@@ -26,4 +33,22 @@ test("solution", () => {
   ]);
 
   expect(solution([[1], [2]], [[3], [4]])).toEqual([[4], [6]]);
+  expect(
+    solution(
+      [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+      ],
+      [
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1]
+      ]
+    )
+  ).toEqual([
+    [2, 3, 4],
+    [5, 6, 7],
+    [8, 9, 10]
+  ]);
 });
