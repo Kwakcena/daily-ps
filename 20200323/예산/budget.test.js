1ebc13254d7444bae408da1849879754c63bba88
Kwakcena <rhkrgudwh@gmail.com>
2020-03-23 16:49:42 +0900

    Reduce 를 이용한 풀이

--- 20200323/예산/budget.test.js
+++ 20200323/예산/budget.test.js
@@ -1,24 +1,9 @@
 const solution = (d, budget) => {
-  let answer = 0;
-  let len = d.length;
-  let remain = budget;
-  //[1,2,3,4,5]
   d.sort((a, b) => a - b);
-  if (d[0] > budget) {
-    return answer;
-  } else {
-    for (let i = 0; i < len; i++) {
-      remain -= d[i];
-      if (remain < 0) {
-        answer = i;
-        break;
-      }
-    }
-    if (answer === 0) {
-      answer = len;
-    }
-    return answer;
+  while (d.reduce((a, b) => a + b, 0) > budget) {
+    d.pop();
   }
+  return d.length;
 };
 
 test("solution", () => {
