d7a886e5d9b6ef6a06897193a6f1707ec2e68233
Kwakcena <rhkrgudwh@gmail.com>
2020-03-23 16:46:10 +0900

    성공) 정렬 후 예산이 가장 적은 부서부터 지원
    예산이 가장 적은 부서부터 지원하다보면 최대 지원을 할 수 있다.
    지원 금액을 다 써버리는 순간 (음수가 되는 순간)의 이전 count 값을
    반환한다.

--- 20200323/예산/budget.test.js
+++ 20200323/예산/budget.test.js
@@ -1,17 +1,25 @@
 const solution = (d, budget) => {
-  const answer = [];
-  getCountersignature(d, 0, 0, budget, 0, answer);
-  return Math.max(...answer);
+  let answer = 0;
+  let len = d.length;
+  let remain = budget;
+  //[1,2,3,4,5]
+  d.sort((a, b) => a - b);
+  if (d[0] > budget) {
+    return answer;
+  } else {
+    for (let i = 0; i < len; i++) {
+      remain -= d[i];
+      if (remain < 0) {
+        answer = i;
+        break;
+      }
+    }
+    if (answer === 0) {
+      answer = len;
+    }
+    return answer;
+  }
 };
-
-function getCountersignature(arr, idx, sum, budget, cnt, answer) {
-  if (sum == budget) {
-    answer.push(cnt);
-  }
-  for (let i = idx; i < arr.length; i++) {
-    getCountersignature(arr, i + 1, sum + arr[i], budget, cnt + 1, answer);
-  }
-}
 
 test("solution", () => {
   expect(solution([1, 3, 2, 5, 4], 9)).toBe(3);
