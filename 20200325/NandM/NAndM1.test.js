3f0f7b72a5e6ce47d3e0567d926ce48d37eda173
Kwakcena <rhkrgudwh@gmail.com>
2020-03-25 17:08:45 +0900

    1부터 N까지 자연수가 들어있는 배열 만들기
    이거 은근히 골치아픈거였는데 이런 방법이 생각났다.

--- 20200325/NandM/NAndM1.test.js
+++ 20200325/NandM/NAndM1.test.js
@@ -1,7 +1,21 @@
-const NAndM = (N, M) => {
+const solution = (N, M) => {
+  const numbers = [...Array(N).fill(1)].map(
+    (number, index) => (number += index)
+  );
   return [1, 2, 3];
 };
 
+function NAndM(digitArray, M) {}
+
+const getNumbers = N =>
+  [...Array(N).fill(1)].map((number, index) => (number += index));
+
 test("N and M 1", () => {
-  expect(NAndM(3, 1)).toEqual([1, 2, 3]);
+  expect(solution(3, 1)).toEqual([1, 2, 3]);
+});
+
+test("1부터 N까지 자연수가 들어있는 배열 만들기", () => {
+  expect(getNumbers(3)).toEqual([1, 2, 3]);
+  expect(getNumbers(4)).toEqual([1, 2, 3, 4]);
+  expect(getNumbers(7)).toEqual([1, 2, 3, 4, 5, 6, 7]);
 });
