b3cec2d14ae1c4ca5e507c585e4ff859b283e3a3
Kwakcena <rhkrgudwh@gmail.com>
2020-03-25 15:53:22 +0900

    Set 자료형을 사용해서 중복을 제거
    combination 함수의 결과는 각 자리수를 사용해 숫자를 만든 것이다.
    011과 11은 둘 다 11로 판단하므로 digit가 2일때 11과 3일때의 11은 같은
    값이다. 따라서 이러한 중복을 제거하기 위해서는 set 자료형을 써야 한다고
    판단되었다.

--- 20200325/find-prime-number/findPrimeNumber.test.js
+++ 20200325/find-prime-number/findPrimeNumber.test.js
@@ -1,10 +1,23 @@
 const solution = numbers => {
+  const EratosArray = Eratosthenes(Array(9999999).fill(1));
+  const answer = new Set();
+
+  for (let digit = 1; digit <= numbers.length; digit++) {
+    combination(
+      numbers.split(""),
+      "",
+      Array(numbers.length).fill(false),
+      digit,
+      answer
+    );
+  }
+  console.log(answer);
   return 3;
 };
 
-function combination(numbersArray, number, checked, digit) {
+function combination(numbersArray, number, checked, digit, answer) {
   if (number.length == digit) {
-    // console.log(number);
+    answer.add(Number(number));
     return;
   }
   let used = Array(numbersArray.length).fill(false);
@@ -15,7 +28,7 @@ function combination(numbersArray, number, checked, digit) {
     checked[i] = true;
     used[numbersArray[i]] = true;
 
-    combination(numbersArray, number, checked, digit);
+    combination(numbersArray, number, checked, digit, answer);
 
     number = number.slice(0, -1);
     checked[i] = false;
@@ -45,7 +58,8 @@ test("만들수 있는 숫자의 조합 구하기", () => {
       "123".split("").sort((a, b) => a - b),
       "",
       [...Array(3).fill(false)],
-      3
+      3,
+      new Set()
     )
   ).toEqual();
 });
