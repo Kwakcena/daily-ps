bb96769f904b28950b65aa496aebe9f80b687890
Kwakcena <rhkrgudwh@gmail.com>
2020-03-26 21:24:55 +0900

    재귀를 이용해서 큰 수를 구한다.
    digits : 입력받은 number를 각 자리수의 값을 원소로 갖는 배열.
    start : 배열의 어떤 원소부터 값으로 담을지 정하는 index
    len : number에서 k개의 수를 제거한 길이
    largest : 가장 큰 수를 저장하는 공간

--- 20200326/큰-수-만들기/makeLargestNumber.test.js
+++ 20200326/큰-수-만들기/makeLargestNumber.test.js
@@ -1,7 +1,16 @@
 const solution = number => {
+  const digits = number.split("");
   return "94";
 };
 
+function largestNumber(digits, start, len, largest) {
+  // TODO
+}
+
 test("solution", () => {
   expect(solution("1924")).toBe("94");
+});
+
+test("get largest number", () => {
+  expect(largestNumber()).toBe();
 });
