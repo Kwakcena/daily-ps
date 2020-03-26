e5bbf47db84cefd199f01568687e680cefdc63ea
Kwakcena <rhkrgudwh@gmail.com>
2020-03-26 12:30:06 +0900

    최소공배수 구하기
    최소공배수를 구하는 함수를 작성했음.

--- 20200326/lcm-n-numbers/LcmNumbers.test.js
+++ 20200326/lcm-n-numbers/LcmNumbers.test.js
@@ -6,9 +6,17 @@ function GCD(a, b) {
   return a % b ? GCD(b, a % b) : b;
 }
 
+const LCM = (a, b, gcd) => (a * b) / gcd;
+
 test("최대공약수(GCD) 구하기", () => {
   expect(GCD(64, 48)).toBe(16);
   expect(GCD(2, 6)).toBe(2);
   expect(GCD(2, 8)).toBe(2);
   expect(GCD(2, 14)).toBe(2);
+});
+
+test("최소공배수(LCM) 구하기", () => {
+  expect(LCM(2, 6, 2)).toBe(6);
+  expect(LCM(6, 8, 2)).toBe(24);
+  expect(LCM(24, 14, 2)).toBe(168);
 });
