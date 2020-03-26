d5307247222eb0429192aa53fa22ffb5982f01b3
Kwakcena <rhkrgudwh@gmail.com>
2020-03-26 21:39:43 +0900

    실패) 테스트케이스 2개 빼고 다 틀렸다.
    시간초과와 실패가 뜬다.

--- 20200326/큰-수-만들기/makeLargestNumber.test.js
+++ 20200326/큰-수-만들기/makeLargestNumber.test.js
@@ -1,16 +1,32 @@
-const solution = number => {
+const solution = (number, k) => {
   const digits = number.split("");
-  return "94";
+  const len = number.length - k;
+  return largestNumber(digits, "", 0, len, [0]);
 };
 
-function largestNumber(digits, start, len, largest) {
-  // TODO
+function largestNumber(digits, numberString, start, len, largest) {
+  if (numberString.length == len) {
+    const number = 0 | numberString;
+    largest[0] = largest[0] < number ? number : largest[0];
+    return;
+  }
+  for (let i = start; i < digits.length; i++) {
+    numberString += digits[i];
+    largestNumber(digits, numberString, i + 1, len, largest);
+    numberString = numberString.slice(0, -1);
+  }
+
+  return largest[0] + "";
 }
 
 test("solution", () => {
-  expect(solution("1924")).toBe("94");
+  expect(solution("1924", 2)).toBe("94");
+  expect(solution("1231234", 3)).toBe("3234");
+  expect(solution("4177252841", 4)).toBe("775841");
 });
 
 test("get largest number", () => {
-  expect(largestNumber()).toBe();
+  expect(largestNumber("1924".split(""), "", 0, 2, [0])).toBe("94");
+  expect(largestNumber("1231234".split(""), "", 0, 4, [0])).toBe("3234");
+  expect(largestNumber("4177252841".split(""), "", 0, 6, [0])).toBe("775841");
 });
