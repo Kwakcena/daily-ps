17b9a0483cee02ddf12b29118a5c18e04c3bddea
Kwakcena <rhkrgudwh@gmail.com>
2020-03-27 18:07:26 +0900

    성공) javascript 에서 풀던 방법으로 풀어 봤다.
    오랜만에 python을 쓰니 힘들구나.

--- 20200326/python/큰수-만들기/make-largest-number_test.py
+++ 20200326/python/큰수-만들기/make-largest-number_test.py
@@ -1,6 +1,23 @@
 def solution(number, k):
-    return "94"
+    startIndex = 0
+    maxIndex = 0
+    answer = ""
+    n = len(number)
+
+    for i in range(k, n):
+        maxChar = '0'
+        for j in range(startIndex, i + 1):
+            if maxChar < number[j]:
+                maxChar = number[j]
+                maxIndex = j
+                if maxChar == '9': break
+        answer += maxChar
+        startIndex = maxIndex + 1
+
+    return answer
 
 
 def test_solution():
     assert solution("1924", 2) == "94"
+    assert solution("4177252841", 4) == "775841"
+    assert solution("1231234", 3) == "3234"
