dba01ed04f56403930231dea18b6bee4803575fb
Kwakcena <rhkrgudwh@gmail.com>
2020-03-27 12:13:13 +0900

    실패) test case 10번
    풀이 방법을 수정했다. 그리디는 현재 상황에서 최선의 선택을 한다는 것을
    잊지말자.
    
    현재 testcase10번만 틀리고 나머지는 성공했다.

--- 20200326/큰-수-만들기/makeLargestNumber.test.js
+++ 20200326/큰-수-만들기/makeLargestNumber.test.js
@@ -1,32 +1,23 @@
 const solution = (number, k) => {
   const digits = number.split("");
-  const len = number.length - k;
-  return largestNumber(digits, "", 0, len, [0]);
+  const len = number.length;
+  let removeCount = k;
+  for (let i = 0; i < len - 1 && removeCount; i++) {
+    while (digits[i] < digits[i + 1] && removeCount) {
+      digits.splice(digits.indexOf(digits[i]), 1);
+      removeCount -= 1;
+      i = 0;
+    }
+  }
+  return removeCount > 0 ? digits.slice(0, len - k).join("") : digits.join("");
 };
-
-function largestNumber(digits, numberString, start, len, largest) {
-  if (numberString.length == len) {
-    const number = 0 | numberString;
-    largest[0] = largest[0] < number ? number : largest[0];
-    return;
-  }
-  for (let i = start; i < digits.length; i++) {
-    numberString += digits[i];
-    largestNumber(digits, numberString, i + 1, len, largest);
-    numberString = numberString.slice(0, -1);
-  }
-
-  return largest[0] + "";
-}
 
 test("solution", () => {
   expect(solution("1924", 2)).toBe("94");
   expect(solution("1231234", 3)).toBe("3234");
   expect(solution("4177252841", 4)).toBe("775841");
-});
-
-test("get largest number", () => {
-  expect(largestNumber("1924".split(""), "", 0, 2, [0])).toBe("94");
-  expect(largestNumber("1231234".split(""), "", 0, 4, [0])).toBe("3234");
-  expect(largestNumber("4177252841".split(""), "", 0, 6, [0])).toBe("775841");
+  expect(solution("12345678", 3)).toBe("45678");
+  expect(solution("87654", 2)).toBe("876");
+  expect(solution("77777", 2)).toBe("777");
+  expect(solution("928763", 3)).toBe("987");
 });
