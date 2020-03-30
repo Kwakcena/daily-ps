6cfb587175d2cac04d0fcbfe70e63b099c753a7f
Kwakcena <rhkrgudwh@gmail.com>
2020-03-30 19:49:54 +0900

    forEach를 이용한 일치하는 선수 지우기
    완주 선수 배열에서 forEach를 돌면서 완주한 선수가 participant에 존재
    하면 splice를 이용해 지우는 방식을 구현함.
    
    효율성 테스트에서 시간 초과로 통과하지 못했다.

--- 3월/20200330/javascript/완주하지_못한_선수/whoHasNotFinished.test.js
+++ 3월/20200330/javascript/완주하지_못한_선수/whoHasNotFinished.test.js
@@ -1,15 +1,20 @@
-const solution = (participant, completion) =>
-  participant.filter(p => (completion.indexOf(p) !== -1 ? false : true));
+const solution = (participant, completion) => {
+  [...completion].forEach(c => {
+    const index = participant.indexOf(c);
+    if (index > -1) participant.splice(index, 1);
+  });
+  return participant[0];
+};
 
 test("solution", () => {
-  expect(solution(["leo", "kiki", "eden"], ["eden", "kiki"])).toEqual(["leo"]);
+  expect(solution(["leo", "kiki", "eden"], ["eden", "kiki"])).toEqual("leo");
   expect(
     solution(
       ["marina", "josipa", "nikola", "vinko", "filipa"],
       ["josipa", "filipa", "marina", "nikola"]
     )
-  ).toEqual(["vinko"]);
+  ).toEqual("vinko");
   expect(
     solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
-  ).toEqual(["mislav"]);
+  ).toEqual("mislav");
 });
