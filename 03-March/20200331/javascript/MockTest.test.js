1bd35ad0eda093cfe34ddbd7fa8160c3492ded72
Kwakcena <rhkrgudwh@gmail.com>
2020-03-31 20:56:41 +0900

    1번 수포자 정답 개수 체크 테스트
    설명은 README 참고

--- 03-March/20200331/javascript/MockTest.test.js
+++ 03-March/20200331/javascript/MockTest.test.js
@@ -2,6 +2,19 @@ const solution = answers => {
   return [1];
 };
 
+const supoja = (answers, supo) => {
+  const len = supo.length;
+  return answers.reduce((count, answer, index) => {
+    return (count += supo[index % len] == answer ? 1 : 0);
+  }, 0);
+};
+
 test("solution", () => {
   expect(solution([1, 2, 3, 4, 5])).toEqual([1]);
+});
+
+test("수포자 1번이 몇개 맞췄는지 반환하는 함수", () => {
+  expect(supoja([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])).toBe(5);
+  expect(supoja([5, 4, 3, 2, 1], [1, 2, 3, 4, 5])).toBe(1);
+  expect(supoja([1, 2, 3, 3, 2, 5, 4, 3, 4, 5], [1, 2, 3, 4, 5])).toBe(6);
 });
