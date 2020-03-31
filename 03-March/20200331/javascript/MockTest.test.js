5b35e6ebb98c4fbd1654c2b26affa96bf59be526
Kwakcena <rhkrgudwh@gmail.com>
2020-03-31 21:22:15 +0900

    Solve 모의고사 problem using JS
    자바스크립트로 모의고사 문제를 풀었다.

--- 03-March/20200331/javascript/MockTest.test.js
+++ 03-March/20200331/javascript/MockTest.test.js
@@ -1,5 +1,14 @@
 const solution = answers => {
-  return [1];
+  const scores = [
+    supoja(answers, [1, 2, 3, 4, 5]),
+    supoja(answers, [2, 1, 2, 3, 2, 4, 2, 5]),
+    supoja(answers, [3, 3, 1, 1, 2, 2, 4, 4, 5, 5])
+  ];
+  const bestScore = Math.max(...scores);
+  return scores.reduce(
+    (_, score, index) => (score == bestScore ? _.concat([index + 1]) : _),
+    []
+  );
 };
 
 const supoja = (answers, supo) => {
@@ -11,6 +20,7 @@ const supoja = (answers, supo) => {
 
 test("solution", () => {
   expect(solution([1, 2, 3, 4, 5])).toEqual([1]);
+  expect(solution([1, 3, 2, 4, 2])).toEqual([1, 2, 3]);
 });
 
 test("수포자 1번이 몇개 맞췄는지 반환하는 함수", () => {
