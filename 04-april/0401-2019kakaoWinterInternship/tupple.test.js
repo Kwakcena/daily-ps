ee9c8872b3221492d141b0c2ce427c189a5bb23d
Kwakcena <rhkrgudwh@gmail.com>
2020-04-02 15:01:04 +0900

    Test) 집합 목록을 2차원 배열로 수정
    배열 안에 집합 목록들을 집합 단위로 갖도록 바꾸는 함수 작성

---
+++ 04-april/0401-2019kakaoWinterInternship/tupple.test.js
@@ -0,0 +1,29 @@
+const solution = s => {
+  return [2, 1, 3, 4];
+};
+
+const getSets = s => {
+  const sets = s.match(/{[\d,]+}/g);
+  return sets.map(set => set.match(/[\d]+,?/g).map(v => parseInt(v)));
+};
+
+test("solution", () => {
+  expect(solution("{{1,2,3},{2,1},{1,2,4,3},{2}}")).toEqual([2, 1, 3, 4]);
+});
+
+test("집합 단위로 추출하기", () => {
+  expect(getSets("{{20,111},{111}}")).toStrictEqual([[20, 111], [111]]);
+  expect(getSets("{{1,2,3},{2,1},{1,2,4,3},{2}}")).toStrictEqual([
+    [1, 2, 3],
+    [2, 1],
+    [1, 2, 4, 3],
+    [2]
+  ]);
+  expect(getSets("{{123}}")).toStrictEqual([[123]]);
+  expect(getSets("{{4,2,3},{3},{2,3,4,1},{2,3}}")).toStrictEqual([
+    [4, 2, 3],
+    [3],
+    [2, 3, 4, 1],
+    [2, 3]
+  ]);
+});
