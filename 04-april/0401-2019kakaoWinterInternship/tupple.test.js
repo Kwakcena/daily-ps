47d849e0d8764aed1558035cccf88087003efec9
Kwakcena <rhkrgudwh@gmail.com>
2020-04-03 20:06:07 +0900

    확장 연산자를 이용해서 더 깔끔하게 바꿈.
    기존의 코드는 n이 0이면, 즉 0번 index 이면 집합들의 첫 번째 배열을
    그대로 반환하도록 했다. 이후에는 filter를 써서 없던 값을 배열에 추가해
    concat으로 이어 붙여 반환했다.
    
    이를 좀 더 깔끔하게 삼항 연산자를 지우고 확장 연산자를 써서 자연스레
    배열을 이어 붙이도록 하여 더욱 깔끔하게 코드를 바꿨다.
    
    - 정렬 기법 또한 함수로 빼다.
    sort 안에 들어가는 함수를 accendingByLength 라는 이름으로 따로 빼서
    전달하면 더욱 깔끔하다.

--- 04-april/0401-2019kakaoWinterInternship/tupple.test.js
+++ 04-april/0401-2019kakaoWinterInternship/tupple.test.js
@@ -15,11 +15,10 @@ const tupple = arr => [
 
 const solution2 = s =>
   JSON.parse(s.replace(/{/g, "[").replace(/}/g, "]"))
-    .sort((a, b) => a.length - b.length)
-    .reduce(
-      (arr, v, n) => (n ? arr.concat(v.filter(f => !arr.includes(f))) : v),
-      []
-    );
+    .sort(accendingByLength)
+    .reduce((acc, cur) => [...acc, ...cur.filter(it => !acc.includes(it))], []);
+
+const accendingByLength = (arr1, arr2) => arr1.length - arr2.length;
 
 test("solution2 다른 사람의 풀이", () => {
   expect(solution2("{{2},{2,1},{2,1,3},{2,1,3,4}}")).toEqual([2, 1, 3, 4]);
