dcafaca53a2ac2e2c03ddb654d5855d9b78d5231
Kwakcena <rhkrgudwh@gmail.com>
2020-04-06 22:49:27 +0900

    banned_id 목록과 일치하는 응모자 아이디 구하기
    getRestrictId 함수로 구현함

--- 04-april/0401-2019kakaoWinterInternship/badUser.test.js
+++ 04-april/0401-2019kakaoWinterInternship/badUser.test.js
@@ -6,6 +6,15 @@ const getBannedReg = (banned_id) => {
   return banned_id.map((id) => id.replace(/\*/g, "[a-z0-9]") + "$");
 };
 
+const getRestrictId = (user_id, banned_id) => {
+  return banned_id.map((id) =>
+    user_id.reduce((banneds, user) => {
+      const matchUser = user.match(id);
+      return matchUser ? banneds.concat(matchUser) : banneds;
+    }, [])
+  );
+};
+
 test("solution", () => {
   expect(
     solution(
@@ -20,4 +29,13 @@ test("baaned_id 목록의 * 을 정규식으로 바꾸기", () => {
     "fr[a-z0-9]d[a-z0-9]$",
     "abc1[a-z0-9][a-z0-9]$",
   ]);
+});
+
+test("바뀐 목록과 일치하는 응모자 아이디의 목록을 구한다.", () => {
+  expect(
+    getRestrictId(
+      ["frodo", "fradi", "crodo", "abc123", "frodoc"],
+      ["fr[a-z0-9]d[a-z0-9]$", "abc1[a-z0-9][a-z0-9]$"]
+    )
+  ).toStrictEqual();
 });
