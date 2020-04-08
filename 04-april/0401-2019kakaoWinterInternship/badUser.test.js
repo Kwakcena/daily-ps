09984a1e6d075ff0e48ec52a8ce765299f4e5557
Kwakcena <rhkrgudwh@gmail.com>
2020-04-08 15:39:26 +0900

    정규식을 이용해 제재 아이디 찾기 테스트
    테스트 통과

--- 04-april/0401-2019kakaoWinterInternship/badUser.test.js
+++ 04-april/0401-2019kakaoWinterInternship/badUser.test.js
@@ -27,14 +27,14 @@ const getBannedReg = (banned_id) => {
   return banned_id.map((id) => `^${id.replace(/\*/g, "[a-z0-9]")}$`);
 };
 
-// const getRestrictId = (user_id, banned_id) => {
-//   return banned_id.map((id) =>
-//     user_id.reduce((banneds, user) => {
-//       const matchUser = user.match(id);
-//       return matchUser ? banneds.concat(matchUser) : banneds;
-//     }, [])
-//   );
-//};
+const getRestrictId = (user_id, banned_id) => {
+  return banned_id.map((id) =>
+    user_id.reduce((banneds, user) => {
+      const matchUser = user.match(id);
+      return matchUser ? banneds.concat(matchUser) : banneds;
+    }, [])
+  );
+};
 
 const getUsedIdObject = (user_id) => {
   return user_id.reduce((obj, id) => {
@@ -97,30 +97,36 @@ test("baaned_id 목록의 * 을 정규식으로 바꾸기", () => {
   ]);
 });
 
-// test("바뀐 목록과 일치하는 응모자 아이디의 목록을 구한다.", () => {
-//   expect(
-//     getRestrictId(
-//       ["frodo", "fradi", "crodo", "abc123", "frodoc"],
-//       ["fr[a-z0-9]d[a-z0-9]$", "abc1[a-z0-9][a-z0-9]$"]
-//     )
-//   ).toStrictEqual([["frodo", "fradi"], ["abc123"]]);
-//   expect(
-//     getRestrictId(
-//       ["frodo", "fradi", "crodo", "abc123", "frodoc"],
-//       [
-//         "fr[a-z0-9]d[a-z0-9]$",
-//         "[a-z0-9]rodo$",
-//         "[a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9]$",
-//         "[a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9]$",
-//       ]
-//     )
-//   ).toStrictEqual([
-//     ["frodo", "fradi"],
-//     ["frodo", "crodo"],
-//     ["abc123", "frodoc"],
-//     ["abc123", "frodoc"],
-//   ]);
-// });
+test("바뀐 목록과 일치하는 응모자 아이디의 목록을 구한다.", () => {
+  expect(
+    getRestrictId(
+      ["frodo", "fradi", "crodo", "abc123", "frodoc"],
+      ["^fr[a-z0-9]d[a-z0-9]$", "^abc1[a-z0-9][a-z0-9]$"]
+    )
+  ).toStrictEqual([["frodo", "fradi"], ["abc123"]]);
+  expect(
+    getRestrictId(
+      ["frodo", "fradi", "crodo", "abc123", "frodoc"],
+      [
+        "^fr[a-z0-9]d[a-z0-9]$",
+        "^[a-z0-9]rodo$",
+        "^[a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9]$",
+        "^[a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9]$",
+      ]
+    )
+  ).toStrictEqual([
+    ["frodo", "fradi"],
+    ["frodo", "crodo"],
+    ["abc123", "frodoc"],
+    ["abc123", "frodoc"],
+  ]);
+  expect(
+    getRestrictId(
+      ["abcdef", "frodoc"],
+      ["^[a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9]$"]
+    )
+  ).toStrictEqual([["abcdef", "frodoc"]]);
+});
 
 test("사용 횟수 객체 구하기", () => {
   expect(
