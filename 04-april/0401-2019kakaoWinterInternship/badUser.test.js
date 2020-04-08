c20eae90d5179556b5ef2f2e0940d4113634ecd3
Kwakcena <rhkrgudwh@gmail.com>
2020-04-08 15:54:08 +0900

    getBannedReg 와 isMatch 합치기
    - 정규 표현식에서 '.'은 하나의 문자와 일치한다는 걸 알았다. [a-z0-9]와
      같이 쓰지 않아도 된다.
    - ban id를 바로 replace로 바꾼 다음 매칭되는 배열만 filter로 걸러 확장
      연산자로 추가하면 간단했다...!

--- 04-april/0401-2019kakaoWinterInternship/badUser.test.js
+++ 04-april/0401-2019kakaoWinterInternship/badUser.test.js
@@ -1,42 +1,17 @@
 const solution = (user_id, banned_id) => {
   return numberOfCases(
     getUsedIdObject(user_id),
-    // getBannedList(user_id, banned_id)
-    getRestrictId(user_id, getBannedReg(banned_id))
+    getBannedList(user_id, banned_id)
   );
 };
 
-const isMatch = (str, pattern) => {
-  if (str.length !== pattern.length) {
-    return false;
-  }
-  return str
-    .split("")
-    .every((it, index) => pattern[index] === "*" || it === pattern[index]);
-};
-
-const getBannedList = (user_id, banned_id) => {
-  return banned_id.reduce((bannnedList, banned) => {
-    return bannnedList.concat([
-      user_id.reduce((userList, user) => {
-        return isMatch(user, banned) ? [...userList, user] : userList;
-      }, []),
-    ]);
-  }, []);
-};
-
-const getBannedReg = (banned_id) => {
-  return banned_id.map((id) => `^${id.replace(/\*/g, "[a-z0-9]")}$`);
-};
-
-const getRestrictId = (user_id, banned_id) => {
-  return banned_id.map((id) =>
-    user_id.reduce((banneds, user) => {
-      const matchUser = user.match(id);
-      return matchUser ? banneds.concat(matchUser) : banneds;
-    }, [])
-  );
-};
+const getBannedList = (users, bans) =>
+  bans
+    .map((ban) => new RegExp(`^${ban.replace(/\*/g, ".")}$`))
+    .reduce((acc, ban) => {
+      const possibleBanUsers = users.filter((user) => user.match(ban));
+      return [...acc, possibleBanUsers];
+    }, []);
 
 const getUsedIdObject = (user_id) => {
   return user_id.reduce((obj, id) => {
@@ -84,50 +59,6 @@ test("solution", () => {
       ["fr*d*", "*rodo", "******", "******"]
     )
   ).toBe(3);
-});
-
-test("baaned_id 목록의 * 을 정규식으로 바꾸기", () => {
-  expect(getBannedReg(["fr*d*", "abc1**"])).toStrictEqual([
-    "^fr[a-z0-9]d[a-z0-9]$",
-    "^abc1[a-z0-9][a-z0-9]$",
-  ]);
-  expect(getBannedReg(["fr*d*", "*rodo", "******", "******"])).toStrictEqual([
-    "^fr[a-z0-9]d[a-z0-9]$",
-    "^[a-z0-9]rodo$",
-    "^[a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9]$",
-    "^[a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9]$",
-  ]);
-});
-
-test("바뀐 목록과 일치하는 응모자 아이디의 목록을 구한다.", () => {
-  expect(
-    getRestrictId(
-      ["frodo", "fradi", "crodo", "abc123", "frodoc"],
-      ["^fr[a-z0-9]d[a-z0-9]$", "^abc1[a-z0-9][a-z0-9]$"]
-    )
-  ).toStrictEqual([["frodo", "fradi"], ["abc123"]]);
-  expect(
-    getRestrictId(
-      ["frodo", "fradi", "crodo", "abc123", "frodoc"],
-      [
-        "^fr[a-z0-9]d[a-z0-9]$",
-        "^[a-z0-9]rodo$",
-        "^[a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9]$",
-        "^[a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9]$",
-      ]
-    )
-  ).toStrictEqual([
-    ["frodo", "fradi"],
-    ["frodo", "crodo"],
-    ["abc123", "frodoc"],
-    ["abc123", "frodoc"],
-  ]);
-  expect(
-    getRestrictId(
-      ["abcdef", "frodoc"],
-      ["^[a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9]$"]
-    )
-  ).toStrictEqual([["abcdef", "frodoc"]]);
 });
 
 test("사용 횟수 객체 구하기", () => {
@@ -178,12 +109,6 @@ test("재귀를 이용해서 경우의 수를 구한다", () => {
       []
     )
   ).toBe(2);
-});
-
-test("정규식 안쓰고 매칭 사용자 찾기", () => {
-  expect(isMatch("frodo", "fr*d*")).toBe(true);
-  expect(isMatch("frodo", "*****")).toBe(true);
-  expect(isMatch("fradi", "c****")).toBe(false);
 });
 
 test("제재 아이디 구하기", () => {
