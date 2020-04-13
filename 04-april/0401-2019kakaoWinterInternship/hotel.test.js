7469ba36a280341b61165b073634db6bd2d58639
Kwakcena <rhkrgudwh@gmail.com>
2020-04-13 20:55:50 +0900

    Fail code) efficiency test case 7 fail
    효율성 테스트 7번 케이스에서 시간 초과가 발생한다.

--- 04-april/0401-2019kakaoWinterInternship/hotel.test.js
+++ 04-april/0401-2019kakaoWinterInternship/hotel.test.js
@@ -1,14 +1,19 @@
-const solution = (k, roomNumber) => {
+const solution = (k, roomNumbers) => {
   const used = {};
-  return roomNumber.map((number) => {
-    while (used[number]) {
-      number += 1;
-    }
-    used[number] = 1;
-    return number;
-  });
+  return roomNumbers.map((number) => findRoom(number, used));
 };
 
+function findRoom(room, used) {
+  if (used[room] == undefined) {
+    used[room] = room + 1;
+    return room;
+  }
+  let next = findRoom(used[room], used);
+  used[room] = next + 1;
+  return next;
+}
+
 test("solution", () => {
-  expect(solution(10, [1, 3, 4, 1, 3, 4])).toStrictEqual([1, 3, 4, 2, 5, 6]);
+  expect(solution(6, [1, 3, 1, 1])).toStrictEqual([1, 3, 2, 4]);
+  expect(solution(10, [1, 3, 4, 1, 3, 1])).toStrictEqual([1, 3, 4, 2, 5, 6]);
 });
