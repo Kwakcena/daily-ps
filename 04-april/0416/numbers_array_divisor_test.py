6eb4af20c768f9adebb98b1f34b8676990a0c737
Kwakcena <rhkrgudwh@gmail.com>
2020-04-16 20:46:01 +0900

    solve 나누어 떨어지는 배열 using python
    파이썬을 이용한 나누어 떨어지는 배열 문제풀이.

---
+++ 04-april/0416/numbers_array_divisor_test.py
@@ -0,0 +1,9 @@
+def solution(arr, divisor):
+    arr = list(filter(lambda x: x % divisor == 0, sorted(arr)))
+    return arr if len(arr) else [-1]
+
+
+def test_solution():
+    assert solution([5, 9, 7, 10], 5) == [5, 10]
+    assert solution([2, 36, 1, 3], 1) == [1, 2, 3, 36]
+    assert solution([3,2,6], 10) == [-1]
