b66ea09301c6249a20ce9b346fd8361d7d0cae12
Kwakcena <rhkrgudwh@gmail.com>
2020-05-05 00:54:27 +0900

    Solve 체육복 problem using js
    자바스크립트를 이용한 체육복 문제 풀이

--- 05-May/0504/체육복/GymClothes.test.js
+++ 05-May/0504/체육복/GymClothes.test.js
@@ -1,22 +1,46 @@
 const solution = (n, losts, reserves) => {
-  const clothes = Array(n).fill(1);
-  reserves.map((reserve) => (clothes[reserve - 1] += 1));
-  losts.map((lost) => (clothes[lost - 1] -= 1));
+  const students = initStudents(n, losts, reserves);
+  for (let i = 0; i < n; i++) {
+    processStudent(students, i);
+  }
+  return students.filter((x) => x >= 0).length;
+};
 
-  for (let i = 0; i < clothes.length; i++) {
-    if (clothes[i] == 2) {
-      if (clothes[i + 1] == 0) {
-        clothes[i] -= 1;
-        clothes[i + 1] += 1;
-      } else if (clothes[i - 1] == 0) {
-        clothes[i] -= 1;
-        clothes[i - 1] += 1;
-      }
+const initStudents = (n, losts, reserves) =>
+  Array(n)
+    .fill(1)
+    .map(
+      (_, index) => reserves.includes(index + 1) - losts.includes(index + 1)
+    );
+
+const processStudent = (students, index) => {
+  if (students[index] >= 0) {
+    return students;
+  }
+  for (const targetIndex of [index - 1, index + 1]) {
+    if (targetIndex < 0 || targetIndex >= students.length) {
+      continue;
+    }
+    if (students[targetIndex] > 0) {
+      students[targetIndex] -= 1;
+      students[index] += 1;
+      return students;
     }
   }
-
-  return clothes.filter((x) => x >= 1).length;
+  return students;
 };
+
+test("옷 빌려오기", () => {
+  expect(processStudent([1, -1, 1, -1, 1], 1)).toStrictEqual([0, 0, 1, -1, 1]);
+  expect(processStudent([0, 0, 1, -1, 1], 2)).toStrictEqual([0, 0, 1, -1, 1]);
+  expect(processStudent([0, 0, 1, -1, 1], 3)).toStrictEqual([0, 0, 0, 0, 1]);
+  expect(processStudent([0, 0, 0, 0, 1], 4)).toStrictEqual([0, 0, 0, 0, 1]);
+});
+test("옷 보유 상태 만들기", () => {
+  expect(initStudents(5, [2, 4], [1, 3, 5])).toStrictEqual([1, -1, 1, -1, 1]);
+  expect(initStudents(5, [2, 4], [3])).toStrictEqual([0, -1, 1, -1, 0]);
+  expect(initStudents(5, [3], [1])).toStrictEqual([1, 0, -1, 0, 0]);
+});
 
 test("solution", () => {
   expect(solution(9, [2, 4, 7, 8], [3, 6, 9])).toBe(8);
