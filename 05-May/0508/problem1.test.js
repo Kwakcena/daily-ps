b63b0128a4735f0be6f3bdca789ffe8a44109f8f
Kwakcena <rhkrgudwh@gmail.com>
2020-05-08 19:13:20 +0900

    Solve 땅따먹기 문제
    풀이 설명은 .md 파일 참고

--- 05-May/0508/problem1.test.js
+++ 05-May/0508/problem1.test.js
@@ -1,64 +1,18 @@
 const solution = (land) => {
-  let sum = 0;
-  for (let i = 0; i < land.length; i++) {
-    const [maxValue, maxIndex] = findMaxValueAndIndex(land[i]);
-    if (i < land.length - 1) {
-      land[i + 1].splice(maxIndex, 1, -1);
+  for (let row = 1; row < land.length; row++) {
+    for (let col = 0; col < land[row].length; col++) {
+      land[row][col] += Math.max(...land[row - 1].filter((_, j) => j !== col));
     }
-    sum += maxValue;
   }
-  return sum;
+  return Math.max(...land[land.length - 1]);
 };
-
-const findMaxValueAndIndex = (arr) => {
-  const max = Math.max(...arr);
-  return [max, arr.findIndex((value) => max == value)];
-};
-
-test("최대값을 찾는다.", () => {
-  expect(findMaxValueAndIndex([1, 2, 3, 5])).toStrictEqual([5, 3]);
-  expect(findMaxValueAndIndex([9, 8, 7, 6])).toStrictEqual([9, 0]);
-  expect(findMaxValueAndIndex([0, 9, 9, 9])).toStrictEqual([9, 1]);
-  expect(findMaxValueAndIndex([9, 1, 1, 1])).toStrictEqual([9, 0]);
-  expect(findMaxValueAndIndex([0, 1, 1, 1])).toStrictEqual([1, 1]);
-  expect(findMaxValueAndIndex([1, 0, 9, 9])).toStrictEqual([9, 2]);
-  expect(findMaxValueAndIndex([1, 9, 0, 9])).toStrictEqual([9, 1]);
-  expect(findMaxValueAndIndex([9, 0, 9, 9])).toStrictEqual([9, 0]);
-});
 
 test("solution", () => {
-  // 이게 핵심이였네...
   expect(
     solution([
       [1, 2, 3, 5],
-      [5, 8, 8, 7],
-      [4, 9999, 2, 1],
+      [3, 8, 9, 7],
+      [2, 20, 99, 3],
     ])
-  ).toBe(17);
-  expect(
-    solution([
-      [1, 2, 3, 5],
-      [5, 6, 7, 8],
-      [4, 3, 2, 1],
-    ])
-  ).toBe(16);
-  expect(
-    solution([
-      [9, 8, 7, 6],
-      [9, 9, 9, 9],
-      [9, 1, 1, 1],
-      [1, 1, 1, 1],
-      [1, 1, 9, 9],
-      [1, 9, 9, 9],
-      [9, 9, 9, 9],
-    ])
-  ).toBe(55);
-  expect(
-    solution([
-      [9, 9, 9, 9],
-      [10, 9, 1, 11],
-      [10, 9, 1, 1000],
-      [9999, 9, 1, 1000],
-    ])
-  ).toBe(1030);
+  ).toBe(112);
 });
