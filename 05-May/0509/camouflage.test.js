ccb83e71ec9a2078a11762b1ee683472433fbc36
Kwakcena <rhkrgudwh@gmail.com>
2020-05-09 20:20:11 +0900

    combination 코드 구현
    nCr = n-1Cr-1 + n-1Cr 이므로 재귀적인 구현이 가능하다.

--- 05-May/0509/camouflage.test.js
+++ 05-May/0509/camouflage.test.js
@@ -10,6 +10,33 @@ const arrayToObject = (clothes) => {
   }, {});
 };
 
+function combination(source, final, arr, n, r, index) {
+  if (r == 0) {
+    final.push(arr);
+  } else if (n == 0 || n < r) {
+    return;
+  } else {
+    arr.push(source[index]);
+    combination(source, final, [...arr], n - 1, r - 1, index + 1);
+    arr.pop();
+    combination(source, final, [...arr], n - 1, r, index + 1);
+  }
+  return final;
+}
+
+test("combination", () => {
+  expect(combination([1, 2, 3, 4, 5], [], [], 3, 2, 0)).toStrictEqual[
+    ([1, 2], [1, 3], [2, 3])
+  ];
+  expect(combination([1, 3, 2], [], [], 3, 1, 0)).toStrictEqual[
+    ([1], [3], [2])
+  ];
+  expect(combination([1, 3, 2], [], [], 3, 2, 0)).toStrictEqual[
+    ([1, 3], [1, 2], [3, 2])
+  ];
+  expect(combination([1, 3, 2], [], [], 3, 3, 0)).toStrictEqual[[1, 3, 2]];
+});
+
 test("solution", () => {
   expect(
     solution([
