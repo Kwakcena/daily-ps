ce7e1ce484c2342c596d3ac15bc5a747885f770b
Kwakcena <rhkrgudwh@gmail.com>
2020-05-09 18:55:37 +0900

    splice를 이용함으로써 나타나는 사이드 이펙트 제어
    기존의 answer 배열은 이전 행에서 최대값을 뽑아 현재 행의 각 열에 더한
    새로운 배열을 계속해서 추가하였다. 이는 점점 쌓여 결국에는 행 * 4의
    개수만큼 배열이 늘어나게 될 것이다.
    
    이럴 필요 없이 크기가 4인 배열에 계속해서 더해 가면 메모리도 아낄 수
    있고 더욱 효율적이다. 여기에 slice(-4) 코드를 지울 수 있다.
    
    slice(-4)는 answer에 추가된 맨 뒤 4개를 뽑아내는 코드인데, 추가하는
    배열이 없으므로 이를 지울 수 있을 것이다.
    
    splice 함수의 매개변수로 count는 없어도 되지 않을까 싶어서 지우고 index
    + 1로 대체했다.

--- 05-May/0508/problem1.test.js
+++ 05-May/0508/problem1.test.js
@@ -1,33 +1,21 @@
-const solution = (land) => {
-  for (let row = 1; row < land.length; row++) {
-    for (let col = 0; col < land[row].length; col++) {
-      land[row][col] += Math.max(...land[row - 1].filter((_, j) => j !== col));
-    }
-  }
-  return Math.max(...land[land.length - 1]);
-};
+const solution = (lands) =>
+  Math.max(
+    ...lands.reduce((arr, row) =>
+      row.map((it, index) => it + Math.max(...splice(arr, index)))
+    )
+  );
 
-const solution2 = (lands) => {
-  const answer = lands.reduce((arr, row) => {
-    return [
-      ...arr,
-      ...row.map(
-        (score, i) =>
-          (score += Math.max(...arr.slice(-4).filter((_, j) => i !== j)))
-      ),
-    ];
-  });
-  return Math.max(...answer);
-};
+const splice = (arr, index) => [
+  ...arr.slice(0, index),
+  ...arr.slice(index + 1),
+  // end를 지정하지 않을 경우 start 부터 끝까지 slice
+];
 
-test("solution2", () => {
-  expect(
-    solution2([
-      [1, 2, 3, 5],
-      [3, 8, 9, 7],
-      [2, 20, 99, 3],
-    ])
-  ).toBe(112);
+test("splice 함수 테스트", () => {
+  expect(splice([1, 2, 3, 5], 0)).toStrictEqual([2, 3, 5]);
+  expect(splice([1, 2, 3, 5], 1)).toStrictEqual([1, 3, 5]);
+  expect(splice([1, 2, 3, 5], 2)).toStrictEqual([1, 2, 5]);
+  expect([1, 2, 3, 4].slice(0, 0)).toStrictEqual([]);
 });
 
 test("solution", () => {
