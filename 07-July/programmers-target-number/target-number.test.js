5ac0ddebeb3cb6c48b60a1a4d5dcd1bb89a19b10
Kwakcena <rhkrgudwh@gmail.com>
2020-07-23 20:18:12 +0900

    Fail] 시간 초과 발생
    테스트 케이스 1,2 에서 시간 초과가 발생.
    
    이전 결과값을 재사용하는 메모이제이션 기법이 필요할 것으로 보임.

--- 07-July/programmers-target-number/target-number.test.js
+++ 07-July/programmers-target-number/target-number.test.js
@@ -1,4 +1,23 @@
-const solution = (numbers, target) => 1;
+let answer = 0;
+
+function dfs(numbers, target, count = 0, number = 0, index = 0) {
+  if (count === numbers.length) {
+    if (number === target) {
+      answer += 1;
+    }
+    return;
+  }
+
+  for (let i = index; i < numbers.length; i += 1) {
+    dfs(numbers, target, count + 1, number + numbers[i], i + 1);
+    dfs(numbers, target, count + 1, number - numbers[i], i + 1);
+  }
+}
+
+const solution = (numbers, target) => {
+  dfs(numbers, target);
+  return answer;
+};
 
 test('solution', () => {
   expect(solution([1, 1, 1, 1, 1], 3)).toBe(5);
